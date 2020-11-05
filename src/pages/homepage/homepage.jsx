169459a65e8c3a2c5332a4b3772271e3430dd218
Astralite <astracodes@protonmail.com>
2020-11-05 17:12:47 +0800

    Break homepage into smaller components, Style menu-item components

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './homepage.scss';
+import Directory from '../../components/directory/directory';
+// import MenuItem from ''
+
+const HomePage = () => {
+  return (
+    <div className='HomePage'>
+      <Directory />
+    </div>
+  )
+}
+
+export default HomePage;
