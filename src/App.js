5f177ff8ce2dfb3fb2dc5641d81beadc5ff122af
Astralite <astracodes@protonmail.com>
2020-11-06 00:41:52 +0800

    Install React Router, Create collection-preview component template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
